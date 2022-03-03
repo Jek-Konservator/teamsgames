9c1e14c4bb9ca635a6dcc8f51bb7766fd6447e31
Кулешов Олег Игоревич <kuleshov_oi@astralnalog.ru>
2022-03-04 00:30:18 +0300

    Добавлены сокеты

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,32 @@ import axios from "axios";
 import getConfig from "next/config";
 import { Provider } from "react-redux";
 import { store } from "../src/toolKitRedux/index";
+import io from "socket.io-client";
 
 const MyApp = ({ Component, pageProps }) => {
   const { publicRuntimeConfig } = getConfig();
+  const socket = io();
+  console.log(socket, "socket");
+  const ltroom = () => {
+    socket.emit("disconnectFromRoom", {
+      msg: "I im disconnected from server",
+    });
+
+    console.log(socket, "alterLeavesocket");
+  };
+  const ctroom = () => {
+    socket.emit("connectToRoom", {
+      msg: "I im connected to server",
+    });
+
+    console.log(socket, "alterConnectsocket");
+  };
+  socket.on("disconnectedFromRoom", ({ msg }) => {
+    console.log(msg, "disconnectedFromRoom");
+  });
+  socket.on("connectedToRoom", ({ msg }) => {
+    console.log(msg, "connectedToRoom");
+  });
 
   axios.create({
     baseURL: publicRuntimeConfig.backendUrl,
@@ -17,6 +40,20 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       <ThemeProvider theme={theme}>
         <Provider store={store}>
+          <button
+            onClick={() => {
+              ltroom();
+            }}
+          >
+            leadve
+          </button>
+          <button
+            onClick={() => {
+              ctroom();
+            }}
+          >
+            connect
+          </button>
           <GlobalStyle />
           <Header />
           <MainContainer>
